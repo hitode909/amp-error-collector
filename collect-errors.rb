23037c91453609a14f7708d8cd406c1c9283c34a
hitode909 <hitode909@gmail.com>
2016-03-23 10:57:18 +0900

    better retry

--- collect-errors.rb
+++ collect-errors.rb
@@ -98,8 +98,12 @@ end
 class AmpValidator
   def validate uri
     LOG.info "validate #{uri}"
-    Retryable.retryable(tries: 10, on: JSON::ParserError) do
-      JSON.parse(`AMP_VALIDATOR_TIMEOUT=60000 node_modules/.bin/amp-validator -o json #{Shellwords.escape uri}`)[uri]
+    Retryable.retryable(tries: 10, on: Exception) do
+      result = JSON.parse(`AMP_VALIDATOR_TIMEOUT=100000 node_modules/.bin/amp-validator -o json #{Shellwords.escape uri}`)[uri]
+      if !result['success'] && result['errors'].length == 0
+        raise 'looks network problem'
+      end
+      result
     end
   end
 end
